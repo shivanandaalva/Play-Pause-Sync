330f39c84b287f98a49e2d1bdb0c3d0328364ef1
alvashivananda11 <alvashivananda11@gmail.com>
2023-09-10 01:02:11 +0530

    deploy

---
+++ server.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var app = express();
+const server = require('http').Server(app);
+var path = require('path');
+var cors = require('cors');
+
+app.use(cors());
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+server.listen(3000);
+
+
+const Server = require('socket.io');
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+    console.log('new user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    socket.on('mydata1', ()=>{
+      console.log("playing");
+      socket.broadcast.emit('event1');
+    });
+
+    socket.on('mydata2', ()=>{
+      console.log("pausing");
+      socket.broadcast.emit('event2');
+    });
+
+  });
+
+
+
